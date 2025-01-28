96780de1a9a90d1c4cd972c0cb84f7b49b4344cb
Samuel Gomes <samuel.gomes.afonso@gmail.com>
2025-01-28 09:33:52 -0500

    refactor OpenGL includes by creating includes.h and updating main.cpp

---
+++ src/includes.h
@@ -0,0 +1,11 @@
+// Freeglut headers
+#include "GL/freeglut.h"
+
+// OpenGL headers
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-#include "GL/freeglut.h"
-#if defined(__APPLE__)
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#endif
+#include "includes.h"
 #include <stdlib.h>
 
 #define WIDTH 1024
